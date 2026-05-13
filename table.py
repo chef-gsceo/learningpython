9be4babff0636253c04cb9f8fb96ef096fd6a33a
CHEF <moltern.light@gmail.com>
2026-05-13 22:16:00 +0300

    finished names

--- table.py
+++ table.py
@@ -4,10 +4,9 @@ for el in names:
     print(el.title())
 
 message = "Warning, "
-
 for el in names:
     print(message + el.title())
-print(len(names))
+print("\tWarned", len(names), "people")
 
 message = "Warning cancel"
 
@@ -17,14 +16,17 @@ names.append('kiki')
 
 for el in names:
     print(message, el.title())
-print(len(names))
+print("\tWarned", len(names), "people")
+
 print("Warning again")
 
-message = "Sorry, "
-
+message = "Sorry, for you warning again,  "
+count = 0
 while len(names) > 2:
     popped_name = names.pop()
     print(message + popped_name.title())
+    count += 1
+print("\tWarned", count, "people")
 
 
 message = "No warning for you, "
